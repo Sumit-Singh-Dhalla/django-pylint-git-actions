55ab7caaedd009d0db757a61f659bf2c61ec429d
Sumit <sumit.singh@kiwitech.com>
2022-01-28 22:13:18 +0530

    added sample complex code to get linter failted

--- my_app/views.py
+++ my_app/views.py
@@ -5,8 +5,20 @@ views file
 from django.http import HttpResponse
 
 
+def complex_code():
+    """ demo complex code to check for the linter to get failed """
+    if True:
+        if True:
+            if True:
+                if True:
+                    if True:
+                        if True:
+                            return True
+
+
 def hello(request):
     """
     simple hello world function based view
     """
+    complex_code()
     return HttpResponse("Hello world!")
